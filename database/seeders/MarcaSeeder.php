a2bcfa27ebb0f8adb4e13c330a0b3fe21504e6f9
Your Name <you@example.com>
2022-05-05 17:07:54 -0500

    evidencia 2

---
+++ database/seeders/MarcaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Marca;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MarcaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        marca::factory(50)->create();  
+    }
+}
